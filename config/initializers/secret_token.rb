fbdbf136868e1c3287c6e377e83d6c3060afe183
dagget20 <juampalg85@gmail.com>
2013-11-04 00:29:17 -0500

    tripea

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripea::Application.config.secret_key_base = '81048205db48d4e56a0ce5f4e1a4c7b054567cf2d25977169988a5d68689c4aea864a6ae2f30af31dabd3b6e3b8044d4899f62f0aa2b08264b5e773e67520157'
