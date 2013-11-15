f12dbc310a26e3d1493452647adefadb9389cc3d
dagget20 <juampalg85@gmail.com>
2013-11-14 23:23:02 -0500

    menu casi listo

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 20131103044411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "landings", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "pages", force: true do |t|
     t.string   "email"
     t.datetime "created_at"
