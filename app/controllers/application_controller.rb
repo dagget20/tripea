a5cc7e60703e01720d871a663d135853fdb85239
dagget20 <juampalg85@gmail.com>
2013-12-04 00:07:25 -0500

    banner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-    before_action :setup_mcapi
 
-  def setup_mcapi
-    @mc = Mailchimp::API.new('7b5bde2b6b1568a1f6579e2f62363eea-us3')
-  end
 end
